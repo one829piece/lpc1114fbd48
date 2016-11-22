d76afb24e5c9481750b312569a9b75038f2dd3b5
one829piece <1103104224@gm.kuas.edu.tw>
2016-11-22 21:44:25 +0800

    +define

--- button.c
+++ button.c
@@ -4,6 +4,13 @@
 #include "gpio.h"
 
 #define TEST_TIMER_NUM		0		/* 0 or 1 for 16-bit timers only */
+#define LED_ON GPIOSetValue( 2, 7, 0 )
+#define LED_OFF GPIOSetValue( 2, 7, 1 )
+#define LED_DIR LPC_GPIO2->DIR|=(1<<7)
+#define BUTTON_DIR	LPC_GPIO2->DIR&=~(1<<6)
+#define BUTTON_press (LPC_GPIO2->DATA&(1<<6))!=(1<<6)
+#define BUTTON_on (LPC_GPIO2->DATA&(1<<6))==(1<<6)
+#define LED_change LPC_GPIO2->DATA^=(1<<7);
 
 extern volatile uint32_t timer16_0_counter;
 extern volatile uint32_t timer16_1_counter;
@@ -11,7 +18,10 @@ extern volatile uint32_t timer16_1_counter;
 int i=0;
 
 int main(void){
-	
+	button_interrupt(250,500);
+}
+
+int button_interrupt(int blinkytime,int presstime){
 	uint32_t interval;
 
 	SystemInit();
@@ -32,37 +42,37 @@ int main(void){
   init_timer16(TEST_TIMER_NUM, interval);
   enable_timer16(TEST_TIMER_NUM);
 	
-	LPC_GPIO2->DIR|=(1<<7);
-	LPC_GPIO2->DIR&=~(1<<6);
+	LED_DIR;
+	BUTTON_DIR;
 	
 	while(1){
-		if((LPC_GPIO2->DATA&(1<<6))!=(1<<6)){
+		if(BUTTON_press){
 			delayMs(1,20);
-			timer16_0_counter = 0;	
+			timer16_0_counter = 0;
 			while(1){
 				delayMs(1,20);
-				if((timer16_0_counter > 500&&(LPC_GPIO2->DATA&(1<<6))!=(1<<6))){
+				if((timer16_0_counter > presstime&&BUTTON_press)){
 					delayMs(1,20);
-					LPC_GPIO2->DATA|=(1<<7);
+					LED_OFF;
 					timer16_0_counter = 0;
 					while(1){
-						if ( (timer16_0_counter > 0) && (timer16_0_counter <= 500)&&(LPC_GPIO2->DATA&(1<<6))!=(1<<6)){
-							GPIOSetValue( 2, 7, 0 );
-						}else if ( (timer16_0_counter > 500) && (timer16_0_counter <= 1000)&&(LPC_GPIO2->DATA&(1<<6))!=(1<<6)){
-							GPIOSetValue( 2, 7, 1 );
-						}else if((timer16_0_counter > 1000)&&(LPC_GPIO2->DATA&(1<<6))!=(1<<6)){
+						if ( (timer16_0_counter > 0) && (timer16_0_counter <= blinkytime)&&BUTTON_press){
+							LED_OFF;
+						}else if ( (timer16_0_counter > blinkytime) && (timer16_0_counter <= blinkytime*2)&&BUTTON_press){
+							LED_ON;
+						}else if((timer16_0_counter > blinkytime*2)&&BUTTON_press){
 							timer16_0_counter = 0;
 						}
 						delayMs(1,20);
-						if((LPC_GPIO2->DATA&(1<<6))==(1<<6)){
-							LPC_GPIO2->DATA|=(1<<7);
+						if(BUTTON_on){
+							LED_OFF;
 							break;
 						}
 					}
 					break;
-				}else if(timer16_0_counter < 500&&(LPC_GPIO2->DATA&(1<<6))==(1<<6)){
+				}else if(timer16_0_counter < presstime&&BUTTON_on){
 					delayMs(1,20);
-					LPC_GPIO2->DATA^=(1<<7);
+					LED_change;
 					break;
 				}
 			}
