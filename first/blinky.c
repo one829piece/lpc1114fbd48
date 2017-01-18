31155dac4c8ba44ef2a8916ba871deb0c413e45a
one829piece <1103104224@gm.kuas.edu.tw>
2017-01-18 22:17:49 +0800

    update

--- first/blinky.c
+++ first/blinky.c
@@ -5,6 +5,20 @@
 volatile uint32_t i = 0;
 
 void blinky(uint32_t portNum, uint32_t bitPosi, uint32_t time){
+
+	SystemInit();
+	
+	/* debug PLL after configuration. */
+	LPC_SYSCON->CLKOUTCLKSEL = 0x03;	/* Select Main clock */
+	LPC_SYSCON->CLKOUTUEN = 0x01;		/* Update clock */
+	LPC_SYSCON->CLKOUTUEN = 0x00;		/* Toggle update register once */
+	LPC_SYSCON->CLKOUTUEN = 0x01;
+	while ( !(LPC_SYSCON->CLKOUTUEN & 0x01) );		/* Wait until updated */
+  	LPC_SYSCON->CLKOUTDIV = 1;			/* Divided by 1 */
+	
+ 	LPC_IOCON->PIO0_1 &= ~0x07;	
+ 	LPC_IOCON->PIO0_1 |= 0x01;		/* CLK OUT */
+
 	GPIOSetDir( portNum, bitPosi, 1 );
 	while(1){
 		GPIOSetValue( portNum, bitPosi, 1 );
--- first/main.c
+++ first/main.c
@@ -2,5 +2,5 @@
 #include "blinky.h"
 
 int main(void){
-	blinky(1,8,1200000);		//blinky(portNum,bitPosi,time);
+	blinky(1,8,800000);		//blinky(portNum,bitPosi,time);
 }
